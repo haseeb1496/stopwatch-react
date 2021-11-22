c94514c3cf86cd9a8ddd4d237cd855a8eda666d3
Haseeb Ullah Usmani <usmani_haseeb@yahoo.com>
2021-11-22 15:54:39 +0500

    Second commit
    Made changes to use react lifecycle hooks effectively.
    
    Signed-off-by: Haseeb Ullah Usmani <usmani_haseeb@yahoo.com>

--- src/layout/stopwatch.js
+++ src/layout/stopwatch.js
@@ -2,16 +2,23 @@ import { useState, useEffect } from 'react';
 import './styles.scss';
 
 function Stopwatch() {
-    const [isActive, setIsActive] = useState(false);
-    const [time, setTime] = useState(0);
-    const [splitTime, setSplitTime] = useState(0)
-    const [entries, setEntries] = useState([]);
+
+    const [state, setState] = useState({
+        isActive: false,
+        time: 0,
+        splitTime: 0,
+        entries: []
+    })
     
     useEffect(() => {
         let interval = null;
-        if (isActive === true) {
+        if (state.isActive === true) {
           interval = setInterval(() => {
-            setTime((time) => time + 11);
+            setState((st)=>({
+                ...st,
+                time: st.time + 11
+            }))
+            
           }, 11);
         } else {
           clearInterval(interval);
@@ -19,24 +26,33 @@ function Stopwatch() {
         return () => {
           clearInterval(interval);
         };
-      }, [isActive]);
+    }, [state.isActive]);
 
     function handleStart() {
-        setIsActive(!isActive);
-        if (isActive) {
-            addEntry(time, 'Pause');
+        setState((st)=>({
+            ...st,
+            isActive: !st.isActive
+        }))
+        if (state.isActive) {
+            addEntry(state.time, 'Pause');
         }
     }
 
     function resetTime() {
-        setTime(0);
-        setSplitTime(0);
-        setEntries([]);
+        setState((st)=>({
+            ...st,
+            time: 0,
+            splitTime: 0,
+            entries: []
+        }))
     }
 
     function handleSplitTime() {
-        setSplitTime(time);
-        addEntry(time, 'Split');
+        setState((st)=>({
+            ...st,
+            splitTime: st.time
+        }))
+        addEntry(state.time, 'Split');
     }
 
     function getFormattedTime(time) {
@@ -44,33 +60,36 @@ function Stopwatch() {
     }
 
     function addEntry(time, type) {
-        setEntries([...entries, {time, type}]);
+        setState((st)=>({
+            ...st,
+            entries:[...st.entries, {time, type}]
+        }))
     }
 
     return (
         <div className='stopwatch-container'>
             <span>
-                {getFormattedTime(time).substring(0, getFormattedTime(time).length -2)}
-                <span className='milliseconds'>{getFormattedTime(time).substring(getFormattedTime(time).length - 2)}</span>
+                {getFormattedTime(state.time).substring(0, getFormattedTime(state.time).length -2)}
+                <span className='milliseconds'>{getFormattedTime(state.time).substring(getFormattedTime(state.time).length - 2)}</span>
             </span>
             
-            <span className='split-time'>{splitTime ? getFormattedTime(splitTime) : 'SPLIT TIME'}</span>
+            <span className='split-time'>{state.splitTime ? getFormattedTime(state.splitTime) : 'SPLIT TIME'}</span>
             <div className='buttons-container'>
-                <div className={'btn ' + (!isActive ? 'start-btn' : 'pause-btn')} onClick={handleStart}> 
-                    <span>{isActive ? 'Pause' : 'Start'}</span>
+                <div className={'btn ' + (!state.isActive ? 'start-btn' : 'pause-btn')} onClick={handleStart}> 
+                    <span>{state.isActive ? 'Pause' : 'Start'}</span>
                 </div>
-                <div className={'btn ' + (!isActive ? 'inactive-btn' : 'split-btn')} onClick={handleSplitTime}>
+                <div className={'btn ' + (!state.isActive ? 'inactive-btn' : 'split-btn')} onClick={handleSplitTime}>
                     <span>Split</span>
                 </div>
-                <div className={'btn ' + (!isActive && time ? 'reset-btn' : 'inactive-btn')} onClick={resetTime}>
+                <div className={'btn ' + (!state.isActive && state.time ? 'reset-btn' : 'inactive-btn')} onClick={resetTime}>
                     <span>Reset</span>
                 </div>
             </div>
-            <div className={!!entries.length ? 'entry-table' : ''}>
-                {entries.map((entry, i) => 
+            <div className={!!state.entries.length ? 'entry-table' : ''}>
+                {state.entries.map((entry, i) => 
                 <span className='entry'>
                     <span className='index'>#{i + 1}</span>
-                    <span className={entry.type === 'Split' ? 'split-time-color' : 'pause-time-color'}>{getFormattedTime(entry.time)}</span>
+                    <span className={entry.type === 'Split' ? 'split-time-color' : 'pause-time-color'}>{getFormattedTime(entry?.time)}</span>
                     <span className='type'>{entry.type}</span>
                 </span>)}
             </div>
